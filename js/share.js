6ac5b4dfe8c4c08334475870df1dec8e457b8058
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2022-10-22 16:36:34 +0900

    modify : kakao share api error

--- js/share.js
+++ js/share.js
@@ -8,39 +8,27 @@ function setShare(){
   const shareImage = url + 'img/image-' + resultAlt + '.png';
   const shareURL = url + 'page/result-' + resultAlt + '.html';
 
-  Kakao.API.request({
-    url: '/v1/api/talk/friends/message/default/send',
-    data: {
-      receiver_uuids: ['${RECEIVER_UUID}'],
-      template_object: {
-        object_type: 'feed',
-        content: {
-          title: shareTitle,
-          description: shareDesc,
-          image_url:
-            shareImage,
-          link: {
-            web_url: shareURL,
-            mobile_web_url: shareURL,
-          },
-        },
-
-        buttons: [
-          {
-            title: '웹으로 보기',
-            link: {
-              mobile_web_url: shareURL,
-              web_url: shareURL,
-            },
-          },
-        ],
+  Kakao.Share.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareDesc,
+      imageUrl:
+        shareImage,
+      link: {
+        mobileWebUrl: shareURL,
+        webUrl: shareURL,
       },
     },
-  })
-  .then(function(response) {
-    console.log(response);
-  })
-  .catch(function(error) {
-    console.log(error);
+
+    buttons: [
+      {
+        title: '웹으로 이동',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+          webUrl: 'https://developers.kakao.com',
+        },
+      },
+    ],
   });
 }
